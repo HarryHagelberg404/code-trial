b331aac54b60c1dbdf8b993343920ffcf892c822
Harry Hagelberg <hh222wf@student.lnu.se>
2021-02-18 10:05:15 +0100

    Adding routes for assumptions

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,19 +5,19 @@ function Footer() {
         <div className="footer-wrapper">
           <div className="footer-fraction">
             <h1 className="footer-column">Client</h1>
-            <a className="footer-column" href="">Assumptions</a>
+            <a className="footer-column" href="https://github.com/HarryHagelberg404/code-trial/blob/main/CLIENT.md">Assumptions</a>
           </div>
           <div className="footer-fraction">
             <h1 className="footer-column">Backend</h1>
-            <a className="footer-column" href="">Assumptions</a>
+            <a className="footer-column" href="https://github.com/HarryHagelberg404/code-trial/blob/main/BACKEND.md">Assumptions</a>
           </div>
           <div className="footer-fraction">
             <h1 className="footer-column">Persistence</h1>
-            <a className="footer-column" href="">Assumptions</a>
+            <a className="footer-column" href="https://github.com/HarryHagelberg404/code-trial/blob/main/PERSISTENCE.md">Assumptions</a>
           </div>
           <div className="footer-fraction">
             <h1 className="footer-column">Testing</h1>
-            <a className="footer-column" href="">Assumptions</a>
+            <a className="footer-column" href="https://github.com/HarryHagelberg404/code-trial/blob/main/TESTING.md">Assumptions</a>
           </div>
         </div>
       </div>
